04fba8b11451d7efd84b5c8fc423668a1fa63ded
Maxwell Cody <maxwell@cody.sh>
2019-10-01 16:42:24 -0500

    improved rank viewer

--- src/Hacks/showranks.cpp
+++ src/Hacks/showranks.cpp
@@ -4,12 +4,8 @@ bool Settings::ShowRanks::enabled = false;
 
 void ShowRanks::CreateMove(CUserCmd* cmd)
 {
-	if (!Settings::ShowRanks::enabled)
-		return;
-
-	if (!(cmd->buttons & IN_SCORE))
-		return;
-
-	float input[3] = { 0.f };
-	MsgFunc_ServerRankRevealAll(input);
+	if(Settings::ShowRanks::enabled && (cmd->buttons & IN_SCORE)) {
+		float input[3] = { 0.f };
+		MsgFunc_ServerRankRevealAll(input);
+	}
 }
